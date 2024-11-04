3c1a50d5f22442fc639ebbe4d6098587a6aa8310
tbonza <tylers.pile@gmail.com>
2024-11-03 17:35:33 -0800

    added streaming file uploads using chunks

--- frontend/src/handler.ts
+++ frontend/src/handler.ts
@@ -1,51 +1,48 @@
 
 const form = document.querySelector("#userinfo") as HTMLFormElement;
 const endpoint = "http://192.168.0.19/api/media/store";
+const chunkSize = 1024 * 1024; // 1MB chunks
+
+async function chunkData(fileItem: File) {
+
+  for (let start = 0; start < fileItem.size; start += chunkSize) {
+
+    const chunk = fileItem.slice(start, start + chunkSize);
+
+    const formData = new FormData();
+    formData.append('chunk', chunk);
+    formData.append("fileName", fileItem.name);
+    formData.append("fileType", fileItem.type);
+    formData.append("fileSize", fileItem.size.toString()); 
+    formData.append('currentChunk', (start / chunkSize).toString());
+    formData.append("fileLastModified", fileItem.lastModified.toString());
+
+    try {
+      const response = await fetch(endpoint, {
+        method: "POST",
+        // Set the FormData instance as the request body
+        body: formData,
+      });
+      console.log(await response.json());
+    } catch (e) {
+      console.error(e);
+    }
+
+  }
+}
 
 async function sendData() {
   // Associate the FormData object with the form element
 
-  const formData = new FormData(form);
   const fileInput = form.querySelector('input[type="file"][multiple]') as HTMLInputElement;
+
   if (fileInput && fileInput.files) {
     for (let i = 0; i < fileInput.files.length; i++) {
-      console.log(fileInput.files[i]);
+
+      const fileItem = fileInput.files[i];
+      await chunkData(fileItem);
     }
   }
-  
-  
-  //console.log(formData.get('file'));
-  //
-  //
-
-  if (fileInput && fileInput.files) {
-    for (let i = 0; i < fileInput.files.length; i++) {
-
-      const formData = new FormData();
-      const fileItem = fileInput.files[i];
-
-      formData.append("name", fileItem.name);
-      formData.append("type", fileItem.type);
-      formData.append("size", fileItem.size.toString()); 
-      formData.append("lastModified", fileItem.lastModified.toString());
-
-      for (const pair of formData.entries()) {
-        console.log(pair[0], pair[1]);
-      }
-
-      try {
-        const response = await fetch(endpoint, {
-          method: "POST",
-          // Set the FormData instance as the request body
-          body: formData,
-        });
-        console.log(await response.json());
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  }                                                     
-  
 }
 
 // Take over form submission
