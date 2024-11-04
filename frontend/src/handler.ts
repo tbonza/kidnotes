94723f93b0ec60544913c831952cc39c3fb2d690
tbonza <tylers.pile@gmail.com>
2024-11-03 16:28:35 -0800

    adding files from form, logging metadata

--- frontend/src/handler.ts
+++ frontend/src/handler.ts
@@ -4,22 +4,48 @@ const endpoint = "http://192.168.0.19/api/media/store";
 
 async function sendData() {
   // Associate the FormData object with the form element
-  const formData = new FormData(form);
-  console.log('form data');
-  for (const key of formData.getHeaders()) {
-    console.log(key);
-  }
 
-  try {
-    const response = await fetch(endpoint, {
-      method: "POST",
-      // Set the FormData instance as the request body
-      body: formData,
-    });
-    console.log(await response.json());
-  } catch (e) {
-    console.error(e);
+  const formData = new FormData(form);
+  const fileInput = form.querySelector('input[type="file"][multiple]') as HTMLInputElement;
+  if (fileInput && fileInput.files) {
+    for (let i = 0; i < fileInput.files.length; i++) {
+      console.log(fileInput.files[i]);
+    }
   }
+  
+  
+  //console.log(formData.get('file'));
+  //
+  //
+
+  if (fileInput && fileInput.files) {
+    for (let i = 0; i < fileInput.files.length; i++) {
+
+      const formData = new FormData();
+      const fileItem = fileInput.files[i];
+
+      formData.append("name", fileItem.name);
+      formData.append("type", fileItem.type);
+      formData.append("size", fileItem.size.toString()); 
+      formData.append("lastModified", fileItem.lastModified.toString());
+
+      for (const pair of formData.entries()) {
+        console.log(pair[0], pair[1]);
+      }
+
+      try {
+        const response = await fetch(endpoint, {
+          method: "POST",
+          // Set the FormData instance as the request body
+          body: formData,
+        });
+        console.log(await response.json());
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }                                                     
+  
 }
 
 // Take over form submission
